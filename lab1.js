40bf43478fa743b1082f615d1abac2503920a9cc
Kateryna Varenko <katyavarenko200408@gmail.com>
2026-02-23 22:22:06 +0200

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,49 @@
+"use strict";
+
+function* daysGenerator() {
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let index = 0;
+
+  while (true) {
+    yield days[index];
+    index++;
+
+    if (index === days.length) {
+      index = 0;
+    }
+  }
+}
+
+function iterateWithTimeout(iterator, seconds) {
+  const startTime = Date.now();
+  let iteration = 0;
+
+  const interval = setInterval(() => {
+    const now = Date.now();
+    if ((now - startTime) / 1000 >= seconds) {
+      console.log("Time is over!");
+      clearInterval(interval);
+      return;
+    }
+
+    const { value, done } = iterator.next();
+    if (done) {
+      clearInterval(interval);
+      return;
+    }
+
+    iteration++;
+    console.log(`Iteration ${iteration}: ${value}`);
+  }, 1000);
+}
+
+const gen = daysGenerator();
+iterateWithTimeout(gen, 10);
