19884e59b5f3fbd154e88d489e6cec23af6e8482
Varenko Kateryna <katyavarenko200408@gmail.com>
2026-03-08 21:07:31 +0200

    Delete lab2/days-generator-lib/src directory

--- lab2/days-generator-lib/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const { daysGenerator } = require("./daysGenerator");
-const { iterateWithTimeout } = require("./iterateWithTimeout");
-
-module.exports = { daysGenerator, iterateWithTimeout };
