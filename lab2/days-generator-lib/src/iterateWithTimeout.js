dfbea06273caf42002bfba5690975b346f7b376e
Varenko Kateryna <katyavarenko200408@gmail.com>
2026-03-11 11:26:14 +0200

    Update iterateWithTimeout.js

--- lab2/days-generator-lib/src/iterateWithTimeout.js
+++ lab2/days-generator-lib/src/iterateWithTimeout.js
@@ -1,31 +1,25 @@
 "use strict";
 
-function iterateWithTimeout(iterator, seconds, onValue, onEnd) {
+function iterateWithTimeout(iterator, seconds) {
   const startTime = Date.now();
   let iteration = 0;
 
   const interval = setInterval(() => {
     const now = Date.now();
     if ((now - startTime) / 1000 >= seconds) {
+      console.log("Time is over!");
       clearInterval(interval);
-      if (onEnd) onEnd("timeout");
-      else console.log("Time is over!");
       return;
     }
 
     const { value, done } = iterator.next();
     if (done) {
       clearInterval(interval);
-      if (onEnd) onEnd("done");
       return;
     }
 
     iteration++;
-    if (onValue) {
-      onValue(iteration, value);
-    } else {
-      console.log(`Iteration ${iteration}: ${value}`);
-    }
+    console.log(`Iteration ${iteration}: ${value}`);
   }, 1000);
 }
 
